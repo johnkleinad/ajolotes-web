5af81f89da426c0ff1074bb346080a7763470610
John Kleinad <juan_mejia13@outlook.com>
2024-11-29 15:09:27 -0600

    add form in index

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Form from './components/Form'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='flex flex-col md:flex-row items-center gap-5 md:gap-10 mb-10'>
-        <img src="/images/gradient_logo.png" alt="" className='max-h-52 h-full' />
+      <div className='flex flex-col items-center gap-5 mb-10'>
+        <img src="/images/gradient_logo.png" alt="" className='max-h-[315px] max-w-72 h-full w-full' />
         <div>
           <span className="text-3xl font-light ">AWS User Group</span> <br />
           <span className="text-5xl font-bold">Ajolotes en la Nube</span>
@@ -16,6 +17,8 @@ function App() {
       <span className='text-2xl font-light'>
         Próximamente nuestra web oficial
       </span>
+
+      <Form />
     </>
   )
 }
